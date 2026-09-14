c56795833083ed7e2c039afabf003cd1751f85c2
priyanshuyadavv12-hub <priyanshuyadavv12@gmail.com>
2026-09-14 10:38:57 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version 
+
+//alert("3+3 ") //we are using nodejs, not browser 
+
+console.log("3+3") //code readability should be high
+console.log("Priyanshu")
+
+let name="Priyanshu yadav"
+let age = 10
+let isLoggedIn = true
+let state = null
+
+//number => 2 to power 53 
+//bigint 
+// string => ""
+//boolean => true/false 
+//null => standalone value 
+// undefined => 
+// symbol => unique 
+
+
+//object 
+console.log(typeof"age")
+console.log(typeof null)
