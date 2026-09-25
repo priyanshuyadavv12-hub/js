8b0c711801221aeb896b97c3d03351cccc2e0496
priyanshuyadavv12-hub <priyanshuyadavv12@gmail.com>
2026-09-25 08:54:39 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,24 @@
+let score= 33
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let  valueInNumber= Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33"=> convert karte hain to it is easily coverts to 
+33 
+//"33abc"=> shows nan
+// true=> 1; false =>0
+
+
+let isLoggedIn = "priyanshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 => shows true; 0 => flase
+// "" => flase
+// "hitesh" => true
+
