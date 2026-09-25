64220b5a010f40484ff09891337978e0b23c033c
priyanshuyadavv12-hub <priyanshuyadavv12@gmail.com>
2026-09-25 09:52:45 +0000

    operations

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score= 33
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let  valueInNumber= Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
 // "33"=> convert karte hain to it is easily coverts to 
 33 
@@ -16,9 +16,50 @@ console.log(valueInNumber)
 let isLoggedIn = "priyanshu"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1 => shows true; 0 => flase
 // "" => flase
 // "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// @@@@@@@@@@@@ Operations @@@@@@@@@@@@@
+
+let value = 3
+let negValue= -value
+console.log(negValue);
+
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2+2)
+// console.log(2**2);
+
+let str1= "Priyanshu"
+let str2= " Yadav"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log("1"+2+ 2)
+// console.log(1+2+"2");
+// console.log( 3+ 4 * 5);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+
 
